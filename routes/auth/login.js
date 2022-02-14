0f8f910087b77edd7d349173fd5e64abfaf11cb9
Beezee_Boi <beezeeboi.znoobtube@gmail.com>
2022-02-14 14:29:55 -0500

    add get login

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const { getUser } = require('../../controllers/user');
+const { getCurrentYear } = require('../../util/getCurrentYear');
 
 const router = express.Router();
 
-router.get('/', ((req, res) => {
+router.get('/', (req, res) => {
+  res.render('auth/login', { title: 'Login', year: getCurrentYear });
+});
+
+router.post('/', ((req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) return res.status(400).json('please enter email and password');
