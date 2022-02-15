7af3b3e16c20277afeb7ba2e61e29bbde78aa1d8
Beezee_Boi <beezeeboi.znoobtube@gmail.com>
2022-02-14 19:52:34 -0500

    bug fix on error handling

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { getUser } = require('../../controllers/user');
 const { getCurrentYear } = require('../../util/getCurrentYear');
-const { userError } = require('user_error');
+const userError = require('./user_error');
 
 const router = express.Router();
 
@@ -9,12 +9,14 @@ router.get('/', (req, res) => {
   res.render('auth/login', { title: 'Login', year: getCurrentYear });
 });
 
-router.post('/', ((req, res) => {
+router.post('/', (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) return res.status(400).json('please enter email and password');
-}));
 
-router.use('/error', (req, res) => userError);
+  getUser(email, password);
+});
+
+router.use('/error', userError);
 
 module.exports = router;
