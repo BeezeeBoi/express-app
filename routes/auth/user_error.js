f4b1e2f9963043dc19aa582e8b345c5672371a09
Beezee_Boi <znoobtube@yahoo.com>
2022-02-15 17:34:57 -0500

    add form and buttons

--- routes/auth/user_error.js
+++ routes/auth/user_error.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   res.render('auth/user_error', { title: 'Error', year: getCurrentYear });
-  next();
 });
 
 module.exports = router;
